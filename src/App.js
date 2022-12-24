76d1ce856caf236677e30262f0832f5784b9593c
Shreeyash <shreeyash545@gmail.com>
2022-12-24 19:04:10 +0530

    Added Navbar n created some componnets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <Navbar />
+        <News />
       </div>
     )
   }
