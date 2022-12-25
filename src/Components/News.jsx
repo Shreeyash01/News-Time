31338fda43450c9baf395a2292bb97fd4c449b2f
Shreeyash <shreeyash545@gmail.com>
2022-12-25 23:17:03 +0530

    created cards for news

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -4,9 +4,22 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is News Component
-        <NewsItem />
+
+      <div className='container my-3'>
+        <h2>Top Headlines -</h2>
+
+        <div className='row my-3'>
+          <div className='col-md-4'>
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className='col-md-4'>
+            <NewsItem />
+          </div>
+          <div className='col-md-4'>
+            <NewsItem />
+          </div>
+        </div>
+
       </div>
     )
   }
