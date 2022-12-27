e34218094b8e1213741f1d2f5575ff22b5dd37de
Shreeyash <shreeyash545@gmail.com>
2022-12-27 19:06:34 +0530

    Added functionality for iterating news array

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -2,6 +2,56 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
+  articles = [
+    {
+      "source": {
+        "id": "bbc-sport",
+        "name": "BBC Sport"
+      },
+      "author": null,
+      "title": "'£10 worth of salmon?' England's cricketers play Secret Santa",
+      "description": "Freya Davies, Alice Davidson-Richards, Nat Sciver and Katherine Brunt share what they would buy their teammates for Secret Santa.",
+      "url": "http://www.bbc.co.uk/sport/cricket/64050783",
+      "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/BAD6/production/_128103874_secretsanta.png",
+      "publishedAt": "2022-12-25T09:22:25.4026845Z",
+      "content": "The build-up to Christmas for England's women is slightly different - and warmer - to usual. They played the last game of their eight-match series against the West Indies on 22 December, and arrive… [+5362 chars]"
+    },
+    {
+      "source": {
+        "id": "espn-cric-info",
+        "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+      "publishedAt": "2020-04-27T11:41:47Z",
+      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    },
+    {
+      "source": {
+        "id": "espn-cric-info",
+        "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+      "publishedAt": "2020-03-30T15:26:05Z",
+      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    }
+  ]
+
+  constructor() {
+    super();
+    this.state = {
+      articles: this.articles,
+      loading: false
+    }
+  }
+
   render() {
     return (
 
@@ -9,15 +59,11 @@ export default class News extends Component {
         <h2>Top Headlines -</h2>
 
         <div className='row my-3'>
-          <div className='col-md-4'>
-            <NewsItem title="myTitle" description="myDesc" imgUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" />
-          </div>
-          <div className='col-md-4'>
-            <NewsItem />
-          </div>
-          <div className='col-md-4'>
-            <NewsItem />
-          </div>
+          {this.state.articles?.map((element) => {
+            return <div className='col-md-4' key={element.url}>
+              <NewsItem title={element.title.slice(0,40)} description={element.description.slice(0,80)} imgUrl={element.urlToImage} newsUrl={element.url} />
+            </div>
+          })}
         </div>
 
       </div>
