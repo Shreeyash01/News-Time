6832af464dd86c14a184509082b023d9c67faccc
Shreeyash <shreeyash545@gmail.com>
2022-12-29 00:03:34 +0530

    Fetched news using api key

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -5,11 +5,11 @@ export default class NewsItem extends Component {
     let { title, description, imgUrl, newsUrl } = this.props
     return (
       <div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={imgUrl} className="card-img-top" alt="newsImg" />
+        <div className="card my-3" style={{ width: "18rem" }}>
+          <img src={!imgUrl?"https://img.etimg.com/thumb/msid-96567765,width-1070,height-580,imgsize-54828,overlay-economictimes/photo.jpg":imgUrl} className="card-img-top" alt="newsImg" />
           <div className="card-body">
-            <h5 className="card-title">{title} ...</h5>
-            <p className="card-text">{description} ...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={newsUrl} target="_blank" rel="noreferrer"  className="btn btn-primary">Read More</a>
           </div>
         </div>
